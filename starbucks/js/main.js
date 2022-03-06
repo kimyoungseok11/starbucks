49fc71ed7cc19d53d6dba48ba5a85e8507345a68
kimyoungseok11 <emaonx2@gmail.com>
2022-03-06 16:40:09 +0900

    22-03-06 header 및 section

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -14,3 +14,21 @@ searchInputEl.addEventListener("blur", () => {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder',"");    
 })
+
+const badgeEl = document.querySelector("header .badges");
+window.addEventListener("scroll", _.throttle(() => {
+    console.log(window.scrollY);
+    if(window.scrollY > 500) {
+        // badgeEl.style.display = 'none'
+        gsap.to(badgeEl, .6, {
+            opacity:0,
+            display:"none",
+        }) //gsap.to(요소, 시간, 옵션);
+    } else {
+        // badgeEl.style.display = 'block'
+        gsap.to(badgeEl, .6, {
+            opacity:1,
+            display:"block"
+        })
+    }
+}, 300)); // _.throttle(함수, 시간) 함수를 입력한 시간만큼 지연시켜서 실행시켜줌
